a6dae89d8bd1fdbccc144b612ba343de39550963
William Schrauwen <williamschrauwen@gmail.com>
2020-10-07 14:33:33 +0200

    User Login Screen & Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,22 +1,30 @@
 import axios from 'axios';
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from '../constants/userConstants';
 
-export const login = (email, password) => async (dispatch) {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_LOGIN_REQUEST
+      type: USER_LOGIN_REQUEST,
     });
     const config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+        'Content-Type': 'application/json',
+      },
+    };
 
-    const { data } = await axios.post('/api/users/login', { email, password}, config);
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
-      payload: data
+      payload: data,
     });
 
     localStorage.setItem('userInfo', JSON.stringify(data));
@@ -29,5 +37,4 @@ export const login = (email, password) => async (dispatch) {
           : error.message,
     });
   }
-
-}
+};
